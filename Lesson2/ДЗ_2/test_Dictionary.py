3a9c64dac544e2711e01e2e7702888e581f88f5e
SmileRexar <53871193+SmileRexar@users.noreply.github.com>
2020-07-23 08:38:22 +0300

    Исправлены замечания по pep8. Добавлен gitignore

--- Lesson2/ДЗ_2/test_Dictionary.py
+++ Lesson2/ДЗ_2/test_Dictionary.py
@@ -9,6 +9,7 @@ def get_dict_mock():
     """
     return {}
 
+
 @pytest.mark.parametrize("input_item , expected_len", [
     ({'iphone 3G': 2008, 'iphone 4S': 2011,
       'iphone 3GS': 2009, 'iphone 5': 2012,
@@ -20,6 +21,7 @@ def get_dict_mock():
 def test_add_items(input_item, expected_len, get_dict_mock):
     get_dict_mock.update(input_item)
     assert len(get_dict_mock) == expected_len
+
 
 def test_delete_item(get_dict_mock):
     get_dict_mock['Test1'] = "1"
